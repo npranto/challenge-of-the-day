50af7316563fbd8358c4e07714a275ce8b62e359
Nazmuz Shakib <Shakib@Nazmuzs-MacBook.local>
2017-07-03 09:24:35 -0400

    adds simpleAdding challenge to day4

---
+++ src/day4/simpleAdding/simpleAdding.spec.js
@@ -0,0 +1,16 @@
+
+// Specs for simpleAdding.js
+
+import {assert} from 'chai';
+import simpleAdding from './simpleAdding.js';
+
+describe('simpleAdding()', () => {
+	it('should return 78 when passed 12', () => {
+		assert.equal(simpleAdding(12), 78);
+	})
+	describe('Different argument type cases', () => {
+		it('should return 9870 when passed 140', () => {
+			assert.equal(simpleAdding(140), 9870);
+		})
+	})	
+})
