f38abc06ee1b6ebbd7270a2ba684b851244ae49b
Nazmuz Pranto <npranto@rosettastone.com>
2017-07-19 08:40:19 -0400

    adds challenge - primeTime

---
+++ src/day14/primeTime/primeTime.spec.js
@@ -0,0 +1,21 @@
+
+// Specs for primeTime.js
+
+import {assert} from 'chai';
+import primeTime from './primeTime.js';
+
+describe('primeTime()', () => {
+    it('should return true when passed 7', () => {
+        assert.equal(primeTime(7), true);
+    })
+    it('should return false when passed 9', () => {
+        assert.equal(primeTime(9), false);
+    })
+    it('should return false when passed 0', () => {
+        assert.equal(primeTime(0), false);
+    })
+    it('should return true when passed 13', () => {
+        assert.equal(primeTime(13), true);
+    })
+})
+
